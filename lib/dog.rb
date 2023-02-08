d34526d8867fac1167cd90b572d9c10d3763d35a
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-08 08:20:53 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+    # name
+  # setter method
+  def name=(name)
+    @name = name
+  end
+
+  # getter method
+  def name
+    @name
+  end
+
+  #breed
+  #setter method
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  #getter method
+  def breed
+    @breed
+  end
+
+
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
+    # name
+    # setter method
+    def name=(name)
+        @name = name
+    end
 
+    # getter method
+    def name
+        @name
+    end
+
+    # job
+    # setter method
+    def job=(job)
+        @job = job
+    end
+    
+    # getter method
+    def job
+        @job
+    end
 end
